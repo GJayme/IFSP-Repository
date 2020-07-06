75e0d15714121a840031694d4f9701e3d0e1e6d6
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-07-06 19:11:19 -0300

    fix: fix ex1

--- ap2/aulas_sincronas/02/ex1.c
+++ ap2/aulas_sincronas/02/ex1.c
@@ -2,12 +2,13 @@
 #include <stdlib.h>
 
 int main(void)
-/*Escreva um programa em C que solicite ao usuário
-um número n e então lê um vetor de n notas e
-calcula a média aritmética.
-- Usar alocação dinâmica do vetor;
-- Liberar a memória ao final;
-- Não limitar o uso da memória.
+/*
+  Escreva um programa em C que solicite ao usuário
+  um número n e então lê um vetor de n notas e
+  calcula a média aritmética.
+  - Usar alocação dinâmica do vetor;
+  - Liberar a memória ao final;
+  - Não limitar o uso da memória.
 */
 {
   int num = 0;
@@ -43,7 +44,7 @@ calcula a média aritmética.
   media = soma/num;
 
   free(notas);
-  printf("A media da turma é %.2f\n", media);
+  printf("A media da turma é %f\n", media);
 
   return 0;
 }
