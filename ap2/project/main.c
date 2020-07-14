21e4a6384ff5399e5278403d555724f938b6d55e
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-07-14 12:37:06 -0300

    feat: menu and adding students

--- ap2/project/main.c
+++ ap2/project/main.c
@@ -11,69 +11,113 @@ void printMenu()
   printf("1 - Cadastrar novo aluno\n");
   printf("2 - Ordenar cadastro de alunos\n");
   printf("3 - Buscar aluno\n");
-  printf("4 - Remover Aluno\n");
+  printf("4 - Remover aluno\n");
   printf("0 - Sair\n");
   printf("----------------------------------\n");
-}
-// FUNCAO CADASTRA ALUNO
-int cadastraAluno()
-{
-  // ESTRUCT DATA
-  struct data_nascimento
-  {
-    int dia;
-    int mes;
-    int ano;
-  };
-
-  // REGISTRA ALUNO
-  struct cadastra_aluno
-  {
-    char nome[TAM_nome];
-    char sobrenome[TAM_sobrenome];
-    struct data_nascimento dataNascimento;
-    int prontuario;
-    char curso;
-  } aluno;
-
-  printf("Nome:\n");
-  scanf("%c", aluno.nome);
-
-  printf("Sobrenome:\n");
-  scanf("%c", aluno.sobrenome);
-
-  printf("Dia de nascimento:\n");
-  scanf("%c", aluno.dataNascimento.dia);
-
-  printf("Mês de nascimento:\n");
-  scanf("%c", aluno.dataNascimento.mes);
-
-  printf("Ano de nascimento:\n");
-  scanf("%c", aluno.dataNascimento.ano);
-
-  printf("Número do prontuario\n");
-  scanf("%c", aluno.prontuario);
-
-  printf("Curso: \n");
-  scanf("%c", aluno.curso);
-
-  return aluno;
 };
+
+// STRUCT DATA
+struct data_nascimento
+{
+  int dia;
+  int mes;
+  int ano;
+};
+
+// STRUCT ALUNO
+struct cadastra_aluno
+{
+  char nome[TAM_nome];
+  char sobrenome[TAM_sobrenome];
+  struct data_nascimento dataNascimento;
+  int prontuario;
+  char curso[4];
+} alunos[1000];
 
 int main()
 {
-  int selecionaOpcao;
-  char aluno;
-  char alunos[1000];
+  int selecionaOpcao, countAlunos = 0;
 
-  printMenu();
-  printf("Selecione uma opção: \n");
-  scanf("%d", &selecionaOpcao);
-  if (selecionaOpcao == 1)
+  while (1)
   {
-    printf("Bem vindo a tela de cadastro de aluno");
-    aluno = cadastraAluno();
-  }
+    printMenu();
+    printf("Selecione uma opção: \n");
+    scanf("%d", &selecionaOpcao);
+    switch (selecionaOpcao)
+    {
+    case 1:
+      if (countAlunos == 0)
+      {
+        printf("Bem vindo a tela de cadastro de aluno.\n");
+        printf("Nome: ");
+        getchar();
+        fgets(alunos[countAlunos].nome, TAM_nome, stdin);
 
+        printf("Sobrenome: ");
+        fgets(alunos[countAlunos].sobrenome, TAM_sobrenome, stdin);
+
+        printf("Dia de nascimento: ");
+        scanf("%d", &alunos[countAlunos].dataNascimento.dia);
+
+        printf("Mês de nascimento: ");
+        scanf("%d", &alunos[countAlunos].dataNascimento.mes);
+
+        printf("Ano de nascimento: ");
+        scanf("%d", &alunos[countAlunos].dataNascimento.ano);
+
+        printf("Número do prontuario ");
+        scanf("%d", &alunos[countAlunos].prontuario);
+
+        printf("Curso: ");
+        getchar();
+        fgets(alunos[countAlunos].curso, 4, stdin);
+      }
+      else
+      {
+        printf("Bem vindo a tela de cadastro de aluno.\n");
+        printf("Nome: ");
+        getchar();
+        fgets(alunos[countAlunos].nome, TAM_nome, stdin);
+
+        printf("Sobrenome: ");
+        fgets(alunos[countAlunos].sobrenome, TAM_sobrenome, stdin);
+
+        printf("Dia de nascimento: ");
+        scanf("%d", &alunos[countAlunos].dataNascimento.dia);
+
+        printf("Mês de nascimento: ");
+        scanf("%d", &alunos[countAlunos].dataNascimento.mes);
+
+        printf("Ano de nascimento: ");
+        scanf("%d", &alunos[countAlunos].dataNascimento.ano);
+
+        printf("Número do prontuario ");
+        scanf("%d", &alunos[countAlunos].prontuario);
+
+        printf("Curso: ");
+        getchar();
+        fgets(alunos[countAlunos].curso, 4, stdin);
+      }
+      countAlunos += 1;
+      printMenu();
+      break;
+    case 2:
+      printf("Tela de ordenação de alunos");
+      break;
+    case 3:
+      printf("Tela de ordenação de pesquisa de alunos");
+      break;
+    case 4:
+      printf("Tela de ordenação de remoção de alunos");
+      break;
+    case 0:
+      printf("Saindo do programa...\n");
+      exit(0);
+      break;
+
+    default:
+      break;
+    }
+  }
   return 0;
 }
