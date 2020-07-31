813a6b90bde30c1e25cb5fb9e0e83f13b52a7112
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-07-31 17:46:11 -0300

    feat: initial merge-sort

--- ap2/projeto02/main.c
+++ ap2/projeto02/main.c
@@ -595,6 +595,44 @@ void selectionSortCurso(aluno *alunos, int numAlunos)
     alunos[m] = temp;
   }
 }
+
+// MERGE-SORT:
+// MERGE-SORT NOME:
+void merge(int *a, int n, int m)
+{
+  int i, j, k;
+  int *x = malloc(n * sizeof(int));
+  for (i = 0, j = m, k = 0; k < n; k++)
+  {
+    x[k] = j == n ? a[i++]
+                  : i == m ? a[j++]
+                           : a[j] < a[i] ? a[j++]
+                                         : a[i++];
+  }
+  for (i = 0; i < n; i++)
+  {
+    a[i] = x[i];
+  }
+  free(x);
+}
+
+void merge_sort(int *a, int n)
+{
+  if (n < 2)
+    return;
+  int m = n / 2;
+  merge_sort(a, m);
+  merge_sort(a + m, n - m);
+  merge(a, n, m);
+}
+// MERGE-SORT SOBRENOME:
+
+// MERGE-SORT DATA DE NASCIMENTO:
+
+// MERGE-SORT PRONTUARIO:
+
+// MERGE-SORTE CURSO:
+
 // BUSCA DE ALUNO:
 // BUSCA POR NOME E SOBRENOME LINEAR:
 void buscarPorNome(aluno *alunos, aluno buscarAluno, int numAlunos)
