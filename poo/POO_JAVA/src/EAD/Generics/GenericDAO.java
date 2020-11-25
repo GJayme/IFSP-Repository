912a7ab6c750d4484f438b076513700a0ddee097
Gabriel Jayme <gabrielj_89@hotmail.com>
2020-11-24 23:20:22 -0300

    feat: Generics

--- poo/POO_JAVA/src/EAD/Generics/GenericDAO.java
+++ poo/POO_JAVA/src/EAD/Generics/GenericDAO.java
@@ -1,2 +1,11 @@
-package EAD.Generics;public interface GenericDAO {
+package EAD.Generics;
+
+import java.util.List;
+
+public interface GenericDAO<T, K> {
+
+    boolean insert(T object);
+    T findOne(K key);
+    List<T> findAll();
+    boolean remove(K key);
 }
